c99a326167a340a4139ce28b3ee037549d738c96
Ufarufa <for_rove@naver.com>
2020-12-14 09:32:39 +0900

    AppBlock  스타일링

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import logo from "./logo.svg";
 import "./App.css";
 import styled, { css } from "styled-components";
+import Buttons from "./components/Buttons";
 
-const Circle = styled.div`
-  width: 5rem;
-  height: 5rem;
-  background: ${(props) => props.color};
-  border-radius: 50%;
-  ${(props) =>
-    props.huge &&
-    css`
-      width: 10rem;
-      height: 10rem;
-    `}
+
+const AppBlock = styled.div`
+  width: 512px;
+  margin: 0 auto;
+  margin-top: 4rem;
+  border: 1px solid black;
+  padding: 1rem;
+
 `;
+
+
 
 function App() {
   return (
-    <>
-      <Circle color="blue" huge></Circle>
-      <Circle color="blue"></Circle>
-    </>
+    <AppBlock>
+      <Buttons>BUTTON</Buttons>
+    </AppBlock>
   );
 }
 
